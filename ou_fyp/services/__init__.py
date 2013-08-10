060b9777275b8a91322c3ee2c712ab171e45cd9b
David_Ng <david.ng.dev@gmail.com>
2013-08-10 22:33:27 +0800

    Make auto increment correct,Add new tags,Add new project

---
+++ ou_fyp/services/__init__.py
@@ -0,0 +1,6 @@
+#from ou_fyp.services.templates.InputForm import * 
+#from ou_fyp.services.AbstractView import *
+#from templates.InputForm import *;
+#from AbstractView import *;
+#from django.contrib.auth.models import User,Group;
+#from django.http import HttpResponse;
